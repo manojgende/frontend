d4398961ce1b344e2c3560406ccd3f686fe6a51f
manoj gowda hr <manojgende2513@gmail.com>
2025-05-14 22:32:35 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,7 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+// api.js
+const BASE = import.meta.env.VITE_BASE_API_URL;
+
+export const USER_API_END_POINT = `${BASE}/user`;
+export const JOB_API_END_POINT = `${BASE}/job`;
+export const APPLICATION_API_END_POINT = `${BASE}/application`;
+export const COMPANY_API_END_POINT = `${BASE}/company`;

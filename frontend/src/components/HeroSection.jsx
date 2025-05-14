3b1e81a68088ae8d7480b0d355b629d5c7cbd977
manoj gowda hr <manojgende2513@gmail.com>
2025-05-14 22:00:43 +0530

    Add files via upload

---
+++ frontend/src/components/HeroSection.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { Button } from './ui/button'
+import { Search } from 'lucide-react'
+import { useDispatch } from 'react-redux';
+import { setSearchedQuery } from '@/redux/jobSlice';
+import { useNavigate } from 'react-router-dom';
+
+const HeroSection = () => {
+    const [query, setQuery] = useState("");
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const searchJobHandler = () => {
+        dispatch(setSearchedQuery(query));
+        navigate("/browse");
+    }
+
+    return (
+<div className='text-center' style={{ color: 'blue' }}>
+            <div className='flex flex-col gap-5 my-10'>
+                <span className=' mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium'>Trusted by Millions, Built for You.</span>
+                <h1 className='text-5xl font-bold'>Find, Apply, Hired. <br /> Your Career, <span className='text-[#6A38C2]'>Your Way.</span></h1>
+                <p><i>From application to offer letter, we’re here to guide you every step of the way.</i></p>
+                <div className='flex w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto'>
+                    <input
+                        type="text"
+                        placeholder='Find Work That Matters to You...'
+                        onChange={(e) => setQuery(e.target.value)}
+                        className='outline-none border-none w-full'
+
+                    />
+                    <Button onClick={searchJobHandler} className="rounded-r-full bg-[#6A38C2]">
+                        <Search className='h-5 w-5' />
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection
